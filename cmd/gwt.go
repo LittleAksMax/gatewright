c2088626b862d762920cf59f0c658fe815c5086d
David R <david2005.rosental@gmail.com>
2026-09-16 15:39:12 +0100

    Root cmd

--- cmd/gwt.go
+++ cmd/gwt.go
@@ -1,4 +1,29 @@
 package cmd
 
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var gwt = &cobra.Command{
+	Use:           "gwt",
+	SilenceErrors: true,
+	Short:         "Simple tool for quickstarting simple projects.",
+	Long:          "Gatewright bootstraps and creates CI for new monorepo projects that deploy as a single Docker container using GH Actions, ECR, and EC2 via SSM.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return errors.New("a subcommand is required")
+	},
+}
+
 func Execute() {
+	if err := gwt.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
 }
